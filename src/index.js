e9a398e3ffbd8414a56e148e02854410b87ac998
Cesar Alva <66078322+cesaralva@users.noreply.github.com>
2023-06-11 11:50:08 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Menu from './menu'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div className='container'>
+    <Menu/>
+  </div>
+);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
