e2195563d8097d0a19948081e331da538d030176
Cesar Alva <66078322+cesaralva@users.noreply.github.com>
2023-06-11 12:03:39 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import Menu from './menu'
-
+import Menu from './Components/Menu';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
